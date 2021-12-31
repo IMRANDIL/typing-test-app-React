f624a2b3cf9b217d715572961d725918a91f7c49
IMRANDIL <aliimranadil2@gmail.com>
2021-12-31 17:02:36 +0530

    yh

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import randomWords from 'random-words';
 
 
@@ -15,10 +15,24 @@ function App() {
   const [countDown, setCountDown] = useState(seconds);
   const [input, setInput] = useState([]);
   const [currentIndex, setCurentIndex] = useState(0);
+  const [inCorrect, setInCorrect] = useState(0);
+  const [correct, setCorrect] = useState(0);
+  const [status, setStatus] = useState('waiting');
+
+  const textInput = useRef(null);
+
 
   useEffect(() => {
     setWords(generateWords())
   }, []);
+
+
+  useEffect(() => {
+    if (status === 'started') {
+      textInput.current.focus()
+    }
+  }, [status])
+
 
 
   function generateWords() {
@@ -29,15 +43,31 @@ function App() {
 
 
   function startAction(prevCount) {
-    const intervalCount = setInterval(() => {
-      setCountDown((prevCount) => {
-        if (prevCount === 0) {
-          clearInterval(intervalCount);
-          setCountDown(countDown)
-        }
-        return prevCount - 1
-      })
-    }, 1000);
+    if (status === 'finished') {
+      setWords(generateWords());
+      setCurentIndex(0);
+      setCorrect(0);
+      setInCorrect(0)
+    }
+
+    if (status !== 'started') {
+      setStatus('started');
+      const intervalCount = setInterval(() => {
+        setCountDown((prevCount) => {
+          if (prevCount === 0) {
+            clearInterval(intervalCount);
+            setCountDown(countDown);
+            setStatus('finished');
+            setInput([])
+
+          }
+          return prevCount - 1
+        })
+      }, 1000);
+    }
+
+
+
 
   }
 
@@ -56,7 +86,13 @@ function App() {
   function checkMatch() {
     const wordToCompare = words[currentIndex];
     const doesItMatch = wordToCompare === input.trim();
-    console.log(doesItMatch);
+    if (doesItMatch) {
+      setCorrect(correct + 1)
+    }
+
+    else {
+      setInCorrect(inCorrect + 1)
+    }
   }
 
 
@@ -75,7 +111,7 @@ function App() {
 
 
       <div className="control is-expanded section">
-        <input type="text" className="input" onKeyDown={listenChar} value={input} onChange={(e) => setInput(e.target.value)} />
+        <input ref={textInput} disabled={status !== 'started'} type="text" className="input" onKeyDown={listenChar} value={input} onChange={(e) => setInput(e.target.value)} />
       </div>
       <div className="section">
         <button className="button is-info is-fullwidth" onClick={startAction}>
@@ -83,41 +119,58 @@ function App() {
         </button>
       </div>
 
+      {status === 'started' && (
 
+        <div className="section">
+          <div className="card">
+            <div className="card-content">
+              <div className="content">
+                {words.map((word, index) => (
 
-      <div className="section">
-        <div className="card">
-          <div className="card-content">
-            <div className="content">
-              {words.map((word, index) => (
+                  <span key={index}>
+                    <span>
+                      {word.split('').map((char, idx) => (
+                        <span key={idx}>{char}</span>
+                      ))}
+                    </span>
 
-                <span key={index}>
-                  <span>
-                    {word.split('').map((char, idx) => (
-                      <span key={idx}>{char}</span>
-                    ))}
+                    <span> </span>
                   </span>
 
-                  <span> </span>
-                </span>
 
-
-              ))}
+                ))}
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className="section">
-        <div className="columns">
-          <div className="column">
-            <p className="is-size-5">
-              Words Per Minute:
-            </p>
-            <p className="has-text-primary is-size-1">{32}</p>
+
+
+      )}
+
+      {status === 'finished' && (
+
+
+        <div className="section">
+          <div className="columns">
+            <div className="column has-text-centered">
+              <p className="is-size-5">
+                Words Per Minute:
+              </p>
+              <p className="has-text-primary is-size-1">{correct}</p>
+            </div>
+            <div className="column has-text-centered">
+              <div className="is-size-5">
+                Accuracy:
+              </div>
+              <p className="has-text-info is-size-1">{Math.round((correct / (correct + inCorrect)) * 100)}%</p>
+            </div>
           </div>
-          <div className="column"></div>
         </div>
-      </div>
+
+
+
+      )}
+
     </div>
   );
 }
