27a7785b4a1846c9850e45bae87cbedc4ff81391
IMRANDIL <aliimranadil2@gmail.com>
2021-12-31 11:13:00 +0530

    yh

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import randomWords from 'random-words';
+
+
+
+
+
+const NumberOfWords = 200;
+const seconds = 60;
+
+
 
 function App() {
+  const [words, setWords] = useState([]);
+  const [countDown, setCountDown] = useState(seconds);
+
+  useEffect(() => {
+    setWords(generateWords())
+  }, []);
+
+
+  function generateWords() {
+    return new Array(NumberOfWords).fill(null).map(() => {
+      return randomWords()
+    })
+  }
+
+
+  function startAction(prevCount) {
+    const intervalCount = setInterval(() => {
+      setCountDown((prevCount) => {
+        if (prevCount === 0) {
+          clearInterval(intervalCount);
+          setCountDown(countDown)
+        }
+        return prevCount - 1
+      })
+    }, 1000);
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="section">
+        <div className="is-size-1 has-text-centered has-text-primary">
+          <h2>{countDown}</h2>
+        </div>
+      </div>
+
+
+      <div className="control is-expanded section">
+        <input type="text" className="input" />
+      </div>
+      <div className="section">
+        <button className="button is-info is-fullwidth" onClick={startAction}>
+          Start Typing
+        </button>
+      </div>
+
+
+
+      <div className="section">
+        <div className="card">
+          <div className="card-content">
+            <div className="content">
+              {words.map((word, index) => (
+                <>
+                  <span key={index}>
+                    {word.split('').map((char, idx) => (
+                      <span key={idx}>{char}</span>
+                    ))}
+                  </span>
+
+                  <span> </span>
+                </>
+
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
